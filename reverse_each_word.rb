f9098664c81bdb44e93515672bc7b7e0fbf30212
Patrick A <akolo@hotmail.com>
2020-03-17 22:32:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,12 +5,13 @@ def reverse_each_word(sentence1)
   i = 0 
   new = sentence1.split(' ')
   
-  new.collect {|i| i.reverse }
-  while i < new.length do
-    rev.push(new[i].reverse)
-    i += 1
-  end 
-  rev.collect.join
-  rev.join(" ")
+  new.collect {|i| i.reverse }.join(" ")
+  
+  # while i < new.length do
+  #   rev.push(new[i].reverse)
+  #   i += 1
+  # end 
+  # rev.collect.join
+  # rev.join(" ")
   binding.pry
 end

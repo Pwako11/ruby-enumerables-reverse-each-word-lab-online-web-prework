3a90dc549930c052f67d5e26336c94a70206a22d
Patrick A <akolo@hotmail.com>
2020-03-17 21:54:45 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+def reverse_each_word(sentence1)
+  rev = []
+  i = 0 
+  new = sentence1.split(' ')
+  
+  while i < new.length do
+    rev.push(new[i].reverse)
+    i += 1
+  end 
+  rev.join(" ")
+end
